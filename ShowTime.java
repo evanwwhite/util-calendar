fe9e98e44f2b9cb67ff9dd47e3c19e08b00f55df
Evan White <69662443+evanwwhite@users.noreply.github.com>
2020-10-16 13:13:34 -0500

    Add files via upload

---
+++ ShowTime.java
@@ -0,0 +1,15 @@
+//Evan White
+import java.util.Calendar;
+public class ShowTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Evan");
+System.out.println(Calendar.getInstance().getTime());
+	}
+
+}
+/*
+Evan
+Thu Sep 03 13:20:50 CDT 2020
+*/
